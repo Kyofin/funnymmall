7943d68ec626c5264fb385c3f0ee09411e30d3a8
peterpoker <1040080742@qq.com>
2018-03-03 01:19:04 +0800

    添加高可用响应

--- src/main/java/com/gec/mmall/common/ServerResponse.java
+++ src/main/java/com/gec/mmall/common/ServerResponse.java
@@ -1,5 +1,67 @@
 package com.gec.mmall.common;
 
 public class ServerResponse<T> {
+	private int code;
+	private String msg;
 	private T data;
+
+	private ServerResponse(int code)
+	{
+		this.code = code;
+	}
+
+	private ServerResponse(int code,String msg)
+	{
+		this.code = code;
+		this.msg = msg;
+	}
+
+	public ServerResponse(int code, T data) {
+		this.code = code;
+		this.data = data;
+	}
+
+	private ServerResponse(int code,String msg,T data)
+	{
+		this.code = code;
+		this.msg = msg;
+		this.data = data;
+	}
+
+
+
+	public boolean isSuccess()
+	{
+		return code == ResponseCode.SUCCESS.getCode();
+	}
+
+	public static <T> ServerResponse<T> createBySuccess(){
+		return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
+	}
+
+	public static <T> ServerResponse<T> createBySuccessMessage(String msg){
+		return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg);
+	}
+
+	public static <T> ServerResponse<T> createBySuccess(T data){
+		return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),data);
+	}
+
+	public static <T> ServerResponse<T> createBySuccess(String msg,T data){
+		return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg,data);
+	}
+
+
+	public static <T> ServerResponse<T> createByError(){
+		return new ServerResponse<T>(ResponseCode.ERROR.getCode(),ResponseCode.ERROR.getDesc());
+	}
+
+
+	public static <T> ServerResponse<T> createByErrorMessage(String errorMessage){
+		return new ServerResponse<T>(ResponseCode.ERROR.getCode(),errorMessage);
+	}
+
+	public static <T> ServerResponse<T> createByErrorCodeMessage(int errorCode,String errorMessage){
+		return new ServerResponse<T>(errorCode,errorMessage);
+	}
 }
